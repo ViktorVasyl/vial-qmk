8f23728ee191e1c9c6c70a12ad262dc2af0c6efa
Viktor <119494805+ViktorVasyl@users.noreply.github.com>
2023-12-07 17:18:20 +0200

    Update config.h

--- keyboards/sofle/rev1/keymaps/vial/config.h
+++ keyboards/sofle/rev1/keymaps/vial/config.h
@@ -26,3 +26,7 @@
 // The four corners
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 5, 3, 8 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 0, 0, 0 }
+
+#define BACKLIGHT_BREATHING
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
